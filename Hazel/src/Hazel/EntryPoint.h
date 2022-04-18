f76a878e0585ed2f371b26707ae4e357e140997c
Erik Lauterwald <92889691+erikrl2@users.noreply.github.com>
2022-04-19 00:16:25 +0200

    add window events

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,7 +7,6 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
-	HZ_CORE_WARN("Initialized Log!");
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
