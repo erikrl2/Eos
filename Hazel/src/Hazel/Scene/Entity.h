089c3a088e561139339265348ea1f2de90c69089
Erik Lauterwald <92889691+erikrl2@users.noreply.github.com>
2022-05-14 17:42:45 +0200

    small fixes

--- Hazel/src/Hazel/Scene/Entity.h
+++ Hazel/src/Hazel/Scene/Entity.h
@@ -43,7 +43,7 @@ namespace Hazel {
 
 		operator bool() const { return m_EntityHandle != entt::null; }
 	private:
-		entt::entity m_EntityHandle = entt::null;
+		entt::entity m_EntityHandle{ entt::null };
 		Scene* m_Scene = nullptr;
 	};
 
