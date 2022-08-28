c724d209f77400518289a01b24e0b67038e55a4b
Erik <92889691+erikrl2@users.noreply.github.com>
2022-08-28 14:37:01 +0200

    Clean up unused code

--- EosEditor/src/EosEditorApp.cpp
+++ EosEditor/src/EosEditorApp.cpp
@@ -17,11 +17,6 @@ namespace Eos {
 
 	Application* CreateApplication(ApplicationCommandLineArgs args)
 	{
-#if 1
-		//args.Args[1] = "assets/scenes/Test.eos";
-		//args.Count++;
-#endif
-
 		ApplicationSpecification spec;
 		spec.Name = "EosEditor";
 		spec.CommandLineArgs = args;
